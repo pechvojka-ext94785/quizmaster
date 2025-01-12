1901da5d65a82e7c0be82d765437cb112aac356d
Karel Smutný <karel.smutny@outlook.com>
2025-01-12 08:42:08 +0000

    Removed forgotten unused util

--- frontend/src/utils/transformObjectToArray.ts
+++
@@ -1,5 +0,0 @@
-export function transformObjectToArray(obj: Record<number, boolean>): number[] {
-    return Object.keys(obj)
-        .filter(key => obj[+key]) // Filter keys where the value is true
-        .map(Number) // Convert keys from string to number
-}
