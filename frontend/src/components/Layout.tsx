117b1eabe381aa79faa64f5403d14faa3aed537f
Karel Smutný <karel.smutny@outlook.com>
2025-01-12 08:38:58 +0000

    Removed forgotten quiz related components

--- frontend/src/components/Layout.tsx
+++
@@ -1,6 +0,0 @@
-import type { JSX } from 'solid-js'
-import './layout.css'
-
-export const Layout = (props: { children: JSX.Element }) => {
-    return <div class="centered-layout">{props.children}</div>
-}
