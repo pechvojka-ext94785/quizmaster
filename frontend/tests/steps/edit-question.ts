e8cbe5387cb54dae93400280044a52090cd38e6d
martinjandik-ext97842 <martin.jandik@ext.csas.cz>
2025-01-16 12:52:17 +0000

    feat(Kralove): add file edit-question, add steps

---
+++ frontend/tests/steps/edit-question.ts
@@ -0,0 +1,24 @@
+import { Before, Then, When } from '@cucumber/cucumber'
+import { CreateQuestionPage } from '../pages'
+import { expectTextToBe, worldAs } from './common'
+import { Question } from './question'
+
+interface EditQuestionWorld {
+    editQuestionPage: CreateQuestionPage
+    bookmarks: Record<string, Question>
+    activeBookmark: string
+}
+
+const world = worldAs<EditQuestionWorld>()
+const openEditPage = async () => world.editQuestionPage.goto()
+Before(() => {
+    world.editQuestionPage = new CreateQuestionPage(world.page)
+})
+When('I start editing question {string}', async (bookmark: string) => {
+    await openEditPage()
+    world.activeBookmark = bookmark
+})
+
+Then('I see the question, answers and explanations', async () => {
+    await expectTextToBe(world.editQuestionPage.questionLocator(), world.bookmarks[world.activeBookmark].question)
+})
--- specs/EditQuestionGui.feature
+++ specs/EditQuestionGui.feature
@@ -9,11 +9,8 @@ Feature: Edit question GUI
       | Prague      | * |
     * saved and bookmarked as "Czechia"
 
-  @focus
+  @ignore
   Scenario: Edit a single-choice question
-    Given I start editing a question
-    When I replace Brno with Ostrava
-    * I save editing question
-    Then I see a link to take the question
-    When I take the question
-    Then I see the question and the answers
+    When I start editing question "Czechia"
+    Then I see the question, answers and explanations
+
