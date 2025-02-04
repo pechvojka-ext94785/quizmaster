069b2b06d08f5b583db29622e2dab3a971cc08e4
Karel Smutný <karel.smutny@outlook.com>
2025-02-04 12:19:01 +0000

    refactor: Broken create question page to components

--- frontend/src/pages/create-question/create-question.tsx
+++ frontend/src/pages/create-question/create-question.tsx
@@ -5,6 +5,15 @@ import { type QuestionData, saveQuestion, getQuestion, updateQuestion } from 'ap
 import { emptyQuestionFormData, toQuestionApiData, toQuestionFormData } from './question-form-data'
 import { QuestionEditForm } from './question-form'
 
+const LoadedIndicator = ({ isLoaded }: { isLoaded: boolean }) => (
+    <input id="is-loaded" type="hidden" value={isLoaded ? 'loaded' : ''} />
+)
+
+const QuestionLink = ({ url }: { url: string }) => url && <span id="question-link">{url}</span>
+
+const ErrorMessage = ({ errorMessage }: { errorMessage: string }) =>
+    errorMessage && <span id="error-message">{errorMessage}</span>
+
 export function CreateQuestionForm() {
     const params = useParams()
     const questionId = params.id ? Number.parseInt(params.id) : undefined
@@ -53,9 +62,9 @@ export function CreateQuestionForm() {
             <h1>Quiz Question Creation Page</h1>
             <h2>If you're happy and you know it create the question</h2>
             <QuestionEditForm questionData={questionData} setQuestionData={setQuestionData} onSubmit={handleSubmit} />
-            {linkToQuestion && <span id="question-link">{linkToQuestion}</span>}
-            {errorMessage && <span id="error-message">{errorMessage}</span>}
-            <input id="is-loaded" type="hidden" value={isLoaded ? 'loaded' : ''} />
+            <ErrorMessage errorMessage={errorMessage} />
+            <QuestionLink url={linkToQuestion} />
+            <LoadedIndicator isLoaded={isLoaded} />
         </div>
     )
 }
