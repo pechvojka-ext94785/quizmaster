ede442d5aa9bd3e8eb6ec1b76d21ca2461e2ae70
Měkuta Kryštof <krmekuta@csas.cz>
2025-02-06 11:18:35 +0000

    fix:format

--- frontend/tests/steps/quiz-score.ts
+++ frontend/tests/steps/quiz-score.ts
@@ -1,16 +1,20 @@
-import {  Given, Then,  } from '@cucumber/cucumber'
-// import { expect } from '@playwright/test'
-import {  World, expectTextToBe } from './common.ts'
+import { Given, Then } from '@cucumber/cucumber'
+import { type World, expectTextToBe } from './common.ts'
 
-Given('I visit the quiz score page', async function(this: World) {
+Given('I visit the quiz score page', async function (this: World) {
     await this.page.goto('/quiz/score')
 })
 
-Then('I should see heading "Quiz Score"', async function(this: World) {
+Then('I should see heading "Quiz Score"', async function (this: World) {
     await expectTextToBe(this.page.locator('h1'), 'Quiz Score')
 })
 
-Then('I should see the text "Your score is 3 correctly answered questions out of 10 which is 30%"', async function(this: World) {
-    await expectTextToBe(this.page.locator('p'), 'Your score is 3 correctly answered questions out of 10 which is 30%')
-})
-
+Then(
+    'I should see the text "Your score is 3 correctly answered questions out of 10 which is 30%"',
+    async function (this: World) {
+        await expectTextToBe(
+            this.page.locator('p'),
+            'Your score is 3 correctly answered questions out of 10 which is 30%',
+        )
+    },
+)
