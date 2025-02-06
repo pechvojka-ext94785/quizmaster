18d42664ca1ff7c1f84e08991dc00f0c69d75f17
Peter Kolárovec <p.kolarovec@gmail.com>
2025-02-06 11:57:12 +0000

    fix: quiz formatting

--- frontend/tests/steps/quiz.ts
+++ frontend/tests/steps/quiz.ts
@@ -1,11 +1,11 @@
-import {  Given, Then,  } from '@cucumber/cucumber'
+import { Given, Then } from '@cucumber/cucumber'
 // import { expect } from '@playwright/test'
-import {  World, expectTextToBe } from './common.ts'
+import { type World, expectTextToBe } from './common.ts'
 
-Given('I visit the quiz page', async function(this: World) {
+Given('I visit the quiz page', async function (this: World) {
     await this.page.goto('/quiz')
 })
 
-Then('I should see heading "Quiz"', async function(this: World) {
+Then('I should see heading "Quiz"', async function (this: World) {
     await expectTextToBe(this.page.locator('h1'), 'Quiz')
 })
