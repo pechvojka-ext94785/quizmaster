7360b16c3255bc10b99aa0d8d65fe9d7ffbaec31
Měkuta Kryštof <krmekuta@csas.cz>
2025-02-06 10:31:48 +0000

    feat(quiz-score) Added quiz score page

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -2,11 +2,13 @@ import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import { QuestionTakePage } from 'pages/question-take'
 import { CreateQuestionForm } from 'pages/create-question'
 import { HomePage } from 'pages/home'
+import { QuizScore } from 'pages/quiz-score'
 
 export const App = () => (
     <BrowserRouter>
         <Routes>
             <Route path="/question/new" element={<CreateQuestionForm />} />
+            <Route path="/quiz/score" element={<QuizScore />} />
             <Route path="/question/:id/edit" element={<CreateQuestionForm />} />
             <Route path="/question/:id" element={<QuestionTakePage />} />
             <Route path="/" element={<HomePage />} />
