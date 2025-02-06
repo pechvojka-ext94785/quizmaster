5b1f8bdb144f127cc374aece062850804d0183f9
svolf-cen57928 <svolf@csas.cz>
2025-02-06 12:57:34 +0000

    correcting locators (#1)

--- frontend/tests/steps/create-question.ts
+++ frontend/tests/steps/create-question.ts
@@ -115,8 +115,8 @@ Then('I see an error message', async () => {
 })
 
 Then('I see 2 answers', async () => {
-    await expect  (world.createQuestionPage.answerTextLocator(0)).toBeVisible()
-    await expect  (world.createQuestionPage.answerTextLocator(1)).toBeVisible()
+    await expect(world.createQuestionPage.answerTextLocator(0)).toBeVisible()
+    await expect(world.createQuestionPage.answerTextLocator(1)).toBeVisible()
 })
 
 Then(/^Multiple choice is (checked|unchecked)$/, async (state: string) => {
