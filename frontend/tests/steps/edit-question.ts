de8a8c20f8dbdf45d683498b8a442c53948ab958
Karel Smutný <karel.smutny@outlook.com>
2025-02-23 09:55:45 +0000

    refactor: unified world.editQuestionPage and createQuestionPage

--- frontend/tests/steps/edit-question.ts
+++ frontend/tests/steps/edit-question.ts
@@ -1,14 +1,11 @@
-import { Before, Then, When } from '@cucumber/cucumber'
+import { Then, When } from '@cucumber/cucumber'
 import { expect } from '@playwright/test'
-import { CreateQuestionPage } from '../pages'
 import { worldAs } from './common'
 import type { QuizmasterWorld } from './world/world'
 
 const world = worldAs<QuizmasterWorld>()
-const openEditPage = async (bookmark: string) => world.editQuestionPage.goto(`${world.bookmarks[bookmark].url}/edit`)
-Before(() => {
-    world.editQuestionPage = new CreateQuestionPage(world.page)
-})
+const openEditPage = async (bookmark: string) => world.createQuestionPage.goto(`${world.bookmarks[bookmark].url}/edit`)
+
 When('I start editing question {string}', async (bookmark: string) => {
     await openEditPage(bookmark)
     world.activeBookmark = bookmark
@@ -17,19 +14,19 @@ When('I start editing question {string}', async (bookmark: string) => {
 Then('I see the question, answers and explanations', async () => {
     await world.page.waitForSelector('#is-loaded[value="loaded"]', { state: 'hidden' })
 
-    const question = await world.editQuestionPage.questionLocator().inputValue()
+    const question = await world.createQuestionPage.questionLocator().inputValue()
     expect(question).toBe(world.bookmarks[world.activeBookmark].question)
 })
 
 When('I change question to {string}', async (newQuestion: string) => {
-    await world.editQuestionPage.questionLocator().fill(newQuestion)
+    await world.createQuestionPage.questionLocator().fill(newQuestion)
 })
 
 When('I save it', async () => {
-    await world.editQuestionPage.submit()
+    await world.createQuestionPage.submit()
 })
 
 Then('I see unchanged url', async () => {
-    const link = await world.editQuestionPage.questionUrlLocator().textContent()
+    const link = await world.createQuestionPage.questionUrlLocator().textContent()
     expect(link).toBe(world.bookmarks[world.activeBookmark].url)
 })
