cb2cab971e4d6d546f513b678e1f1e974c0803d7
Chovanec Daniel <dchovanec@csas.cz>
2025-03-25 11:48:29 +0000

    fix lint format

--- frontend/specs/steps/quiz-score.ts
+++ frontend/specs/steps/quiz-score.ts
@@ -1,16 +1,16 @@
 import { Given, Then, When } from './fixture.ts'
 
-  Given('a quiz with 10 questions', async () => {
+Given('a quiz with 10 questions', async () => {
     // Step: Given a quiz with 10 questions
     // From: specs/QuizScore.feature:4:5
-  });
+})
 
-  When('I answer 5 questions correctly and 5 questions incorrectly', async () => {
+When('I answer 5 questions correctly and 5 questions incorrectly', async () => {
     // Step: When I answer 5 questions correctly and 5 questions incorrectly
     // From: specs/QuizScore.feature:5:5
-  });
+})
 
-  Then('I see the score', async () => {
+Then('I see the score', async () => {
     // Step: Then I see the score
     // From: specs/QuizScore.feature:6:5
-  });
+})
